2c3b3c10af1ef618537db16a33155b6cece90e47
Karsten Bolding <karsten@bolding-bruggeman.com>
2023-03-17 16:40:25 +0100

    added working CMakeLists.txt - simplified score.pyx

--- python/pyensdam/__init__.py
+++ python/pyensdam/__init__.py
@@ -4,10 +4,10 @@ A simple, minimal example of building a Python C module using CMake.
 
 __version__ = '0.1.2'
 
-from . import anam
+#from . import anam
 #from . import augm
 from . import score
-from . import stat
+#from . import stat
 #from . import update
 #from . import interptools
 #from . import obserror
--- python/src/score.pyx
+++ python/src/score.pyx
@@ -18,42 +18,32 @@ cdef extern void c_compute_ranks_histogram(int nstate, int nens, double* ens, do
 # From score_crps
 #KB crps_missing_value = score_crps.ensdam_score_crps.crps_missing_value
 
-def crps_score_global(ens not None, verif not None):
-    cdef double[:,::1] ens_ = ens
-    cdef double[::1] verif_ = verif
-
+def crps_score_global(double[:,::1] ens not None, double[::1] verif not None):
     cdef double crps
     cdef double reliability
     cdef double resolution
 
-    c_crps_score_global(<int>ens_.shape[0], <int>ens_.shape[1], &crps, &reliability, &resolution, &ens_[0][0], &verif_[0])
+    c_crps_score_global(<int>ens.shape[0], <int>ens.shape[1], &crps, &reliability, &resolution, &ens[0,0], &verif[0])
     return crps, reliability, resolution
 
-def rcrv_score_global(ens not None, verif not None):
-    cdef double[:,::1] ens_ = ens
-    cdef double[::1] verif_ = verif
-
+def rcrv_score_global(double[:,::1] ens not None, double[::1] verif not None):
     cdef double ens_bias
     cdef double ens_spread
 
-    c_rcrv_score_global(<int>ens_.shape[0], <int>ens_.shape[1], &ens_bias, &ens_spread, &ens_[0][0], &verif_[0])
+    c_rcrv_score_global(<int>ens.shape[0], <int>ens.shape[1], &ens_bias, &ens_spread, &ens[0,0], &verif[0])
     return ens_bias, ens_spread
 
-def compute_ranks(ens not None, verif not None, histogram=False):
-    cdef double[:,::1] ens_ = ens
-    cdef double[::1] verif_ = verif
-
-    ranks = numpy.zeros((<int>ens_.shape[0]), dtype=numpy.intc)
+def compute_ranks(double[:,::1] ens not None, double[::1] verif not None, histogram=False):
+    ranks = numpy.zeros((<int>ens.shape[0]), dtype=numpy.intc)
     cdef int[::1] ranks_ = ranks
-
-    rank_histogram = numpy.zeros((<int>ens_.shape[1]+1), dtype=numpy.intc)
+    rank_histogram = numpy.zeros((<int>ens.shape[1]+1), dtype=numpy.intc)
     cdef int[::1] rank_histogram_ = rank_histogram
 
     if histogram:
-        c_compute_ranks_histogram(<int>ens_.shape[0], <int>ens_.shape[1], &ens_[0][0], &verif_[0], &ranks_[0], &rank_histogram_[0])
+        c_compute_ranks_histogram(<int>ens.shape[0], <int>ens.shape[1], &ens[0,0], &verif[0], &ranks_[0], &rank_histogram_[0])
         return ranks, rank_histogram
     else:
-        c_compute_ranks(<int>ens_.shape[0], <int>ens_.shape[1], &ens_[0][0], &verif_[0], &ranks_[0])
+        c_compute_ranks(<int>ens.shape[0], <int>ens.shape[1], &ens[0,0], &verif[0], &ranks_[0])
         return ranks
 
 
